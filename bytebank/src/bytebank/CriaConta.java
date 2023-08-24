c297a6c854130a9cb45306e72da3cc823fd6b379
Priscila Rossetto <priscilassouzap@gmail.com>
2023-08-24 17:51:06 -0300

    Testando referências e métodos

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -5,5 +5,24 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+		
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println("A primeira conta tem " + primeiraConta.saldo);
+		System.out.println("A segunda conta tem " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia = 146;
+		System.out.println("agora a segunda conta esta" 
+						+ " na agencia " + segundaConta.agencia);
+		
 	}
 }
---
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,25 @@
+package bytebank;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDaPriscila = new Conta();
+		contaDaPriscila.saldo = 100;
+		
+		contaDaPriscila.deposita(50);
+		System.out.println(contaDaPriscila.saldo);
+		
+		contaDaPriscila.saca(20);
+		System.out.println(contaDaPriscila.saldo);
+		
+		Conta contaDoJoao = new Conta();
+		contaDoJoao.deposita(1000);
+		
+		if(contaDoJoao.transfere(300, contaDaPriscila)) {
+			System.out.println("transferencia com sucesso");
+		}else {
+			System.out.println("faltou dinheiro");
+		}
+		System.out.println(contaDoJoao.saldo);
+		System.out.println(contaDaPriscila.saldo);
+	}
+}
