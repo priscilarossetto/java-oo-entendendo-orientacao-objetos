c24d70a69bdae5d004b56fd6168885d4a4553b80
Priscila Rossetto <priscilassouzap@gmail.com>
2023-09-05 19:55:34 -0300

    construtores e membros estáticos

--- src/Conta.java
+++ src/Conta.java
@@ -1,11 +1,18 @@
-
-
 public class Conta {
 	
 	private double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int total;
+	
+	public Conta(int agencia, int numero) {
+		total++;
+		System.out.println("o total de contas é " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("estou criando uma conta " + this.numero);
+	}
 	
 	
 	public void deposita(double valor) {
@@ -39,6 +46,10 @@ public class Conta {
 	}
 	
 	public void setNumero(int numero){
+		if(numero <= 0) {
+			System.out.println("não pode valor <= 0");
+			return;
+		}
 		this.numero = numero;
 	}
 	
@@ -47,7 +58,22 @@ public class Conta {
 	}
 	
 	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("não pode valor menor igual a 0");
+			return;
+		}
 		this.agencia = agencia;
 	}
 	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
+	}
 }
--- src/TestaGetESet.java
+++ src/TestaGetESet.java
@@ -1,9 +1,24 @@
-
 public class TestaGetESet {
 	
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.setNumero(1337);
+		Conta conta = new Conta(1337, 24226);
+
 		System.out.println(conta.getNumero());
+		
+		Cliente priscila = new Cliente();
+		priscila.setNome("priscila rossetto");
+		
+		conta.setTitular(priscila);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programadora");
+		// agora em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programadora");
+		
+		System.out.println(titularDaConta);
+		System.out.println(priscila);
+		System.out.println(conta.getTitular());
 	}
 }
