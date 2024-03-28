ada23f89b93bc0c25a294c4eebe0e6e7db795333
Priscila Rossetto <priscilassouzap@gmail.com>
2024-03-28 17:28:23 -0300

    Basico sobre Java

---
+++ CursoPOO/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+		
+		byte variavelByte = 120;
+		//short -32768 a 32767
+		//char 0 a 65535
+		//int -2b a 2b
+		//long -9t a 9t
+		
+		int variavelInt = 2147483;
+				
+		System.out.println(variavelInt);
+		
+		
+
+	}
+
+}
