0d958ee5d5a1d064443b3497a23433455caca8ef
Priscila Rossetto <Priscila Rossetto@DESKTOP-F3PL1T0>
2024-04-14 18:02:08 -0300

    modelagem Orientada a Objetos

--- POO/src/Animais/Cachorro.java
+++ POO/src/Animais/Cachorro.java
@@ -2,6 +2,8 @@ package Animais;
 
 public class Cachorro {
 	//atributos
+	
+	static int numeroDeCachorros;
 	private String nome;
 	private String cor;
 	private int altura;
@@ -20,13 +22,25 @@ public class Cachorro {
 		this.peso = peso;
 		this.tamanhoDoRabo = tamanhoDoRabo;
 		this.estadoDeEspirito = estadoDeEspirito;
+		
+		numeroDeCachorros ++;
 	}
 
 	//metodos
+	
+	
 	public String getNome() {
 		return this.nome;
 	}
 	
+	public static int getNumeroDeCachorros() {
+		return numeroDeCachorros;
+	}
+
+	public static void setNumeroDeCachorros(int numeroDeCachorros) {
+		Cachorro.numeroDeCachorros = numeroDeCachorros;
+	}
+
 	public void setNome(String nome) {
 		//com o this eu mostro para o java que eu quero o atributo dessa classe
 		//as cores no eclipse "mostram" isso na prática
@@ -95,4 +109,11 @@ public class Cachorro {
 
 		return estadoDeEspirito;
 	}
+
+	@Override
+	public String toString() {
+		return "Cachorro [nome=" + nome + "]";
+	}
+	
+	
 }
--- POO/src/Main.java
+++ POO/src/Main.java
@@ -5,24 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//criei um objeto!
-		Cachorro cachorro1 = new Cachorro();
+		Cachorro cachorro1 = new Cachorro("Milka", "Marrom", 65, 16.0, 10, "nada");
+		
+		System.out.println(cachorro1.getNumeroDeCachorros());
+		
 		Cachorro cachorro2 = new Cachorro("Maby", "Creme", 25, 5.5, 5, "nada");
-	
-		System.out.println(cachorro2.getNome());
-		System.out.println(cachorro2.getPeso());
 		
-		//executei um método do objeto, uma ação!
-		cachorro1.latir();
-		
-		cachorro1.pegar();
-		
-		//imprime o retorno do método pegar()
-		System.out.println("O cachorro pegou uma " + cachorro1.pegar());
-		
-		System.out.println("O cachorro está " + cachorro1.interagir("nada"));
-		System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
-		System.out.println("O cachorro está " + cachorro1.interagir("vai dormir!"));
-		
+		System.out.println(cachorro2.getNumeroDeCachorros());
+		System.out.println(cachorro1.getNumeroDeCachorros());
 		
 	}
 
