d943ed1a2fc6567021e45730ab102c7f435d5dca
Priscila Rossetto <Priscila Rossetto@DESKTOP-F3PL1T0>
2024-04-17 17:13:39 -0300

    info importantes sobre java

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -9,14 +9,29 @@ public class Main {
 		
 		//criei um objeto!
 		Cachorro cachorro1 = new Cachorro("Milka", "Marrom", 65, 16.0, 10, "nada");
-		
 		Gato gato1 = new Gato("Felix", "Preto", 4.5);
-		
 		Passaro passaro1 = new Passaro("Frajola", "Amarelo", 0.5);
-		
 		cachorro1.soar();
 		gato1.soar();
 		passaro1.soar();
+		
+		int _$teste_$ = 2;
+		double ______ = 23.3;
+		
+		int inteiro; //int, long, byte = 0;
+		float $$; // double e float = 0.0
+		boolean $87; //boolean tem como default false
+		char ch; // char é vazio
+		String teste; // valor default de qualquer objeto é null
+		
+		//casting quando o Java não consegue mudar uma coisa para outra e você indica isso para ele.
+		//quando um valor maior  para transformar em algo menor, precisa do casting.
+		double d = 5.5d;
+		float f = 3.00f;
+		
+		float x = f + (float) d;
+		
+		System.out.println(x);
 	}
 
 }
